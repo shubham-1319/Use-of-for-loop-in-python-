0f6d00eb8808a786e83a2fd09af2c3c0148cfe7e
ShubhaM <81143367+shubham-1319@users.noreply.github.com>
2023-12-12 23:49:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def print_numbers():
+    for i in range(1, 11):
+        print(i)
+
+def repeat_sequence(num_times):
+    for _ in range(num_times):
+        print_numbers()
+
+# Repeat the sequence 3 times (you can change the value as needed)
+repeat_sequence(3)
